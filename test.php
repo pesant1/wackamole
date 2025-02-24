b4bf01207b8185308b4aebfacceb190e78c02ca0
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-02-24 14:19:17 -0500

    Update test.php

--- test.php
+++ test.php
@@ -41,7 +41,12 @@ table, th, td
             { 
                 while($row = $result->fetch_assoc())
                 {
-                    echo "<tr><td> {$row["name"]} </td><td> score: {$row["score"]} </td><br>";
+                    echo
+                    "<tr><td> 
+                        {$row["name"]} 
+                    </td><td>
+                        {$row["score"]} 
+                    </td><br>";
                 }
             }
             mysqli_close($conn); 
