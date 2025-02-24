20bd27643b877fc294682b652f41fff4167532d9
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-02-24 12:32:53 -0500

    Update test.php

--- test.php
+++ test.php
@@ -8,5 +8,32 @@
 <h1>This is a Heading</h1>
 <p>This is a paragraph.</p>
 
+<?php
+        $server = "localhost";
+        $username = "php";
+        $password = "php1";
+        $database = "finalproject";
+        $conn = mysqli_connect($server, $username, $password, $database);
+
+        //Connection checker
+        if (!$conn)
+        {
+            die("Connection failed: {mysqli_connect_error()}");
+            echo "Connected successfully";
+        }
+        $sql = "select * from test;";
+        $result = mysqli_query($conn, $sql);
+    ?>
+
+<body style="background-color:gray;">
+
 </body>
 </html> 
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Final Project test sql + php</title>
+</head>
+<body>
+
