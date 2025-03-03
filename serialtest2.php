c7a3604b3a95f0aade1854c61ff0fc38a3f07861
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-03 14:52:02 -0500

    Update serialtest2.php

--- serialtest2.php
+++ serialtest2.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 <body style="background-color:gray;">
-<h1>Test file to send serial from pi to avr</h1>
+<h1>Test file to send serial from pi to avr with button</h1>
 
     <form action = "serialtest.php" method="POST">
             <button type = "submit" Send message></button>
