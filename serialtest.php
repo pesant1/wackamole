64dfb7f3337821b0ef572f36aff5f1d0b2862a4f
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-03 13:30:41 -0500

    Create serialtest.php

---
+++ serialtest.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Test serial txd file</title>
+
+</head>
+<body>
+<body style="background-color:gray;">
+<h1>Test file to send serial from pi to avr</h1>
+
+
+</body>
+</html> 
+
