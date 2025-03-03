92bfa81b8240186be6556e7a26636a8ab3ea713e
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-03 14:14:05 -0500

    Update serialtest.php

--- serialtest.php
+++ serialtest.php
@@ -9,6 +9,19 @@
 <body style="background-color:gray;">
 <h1>Test file to send serial from pi to avr</h1>
 
+<?php
+        $serialPort = "/dev/ttyUSB0"
+        $baudrate = "2400"  
+        
+        $fp = $fopen($serialPort, "w+");
+        exec("ssty -F $serialPort $baudrate cs8 -cstopb -parenb");
+
+        $datasend = "Goodbye World\n";
+        fwrite($fp, $daasend);
+
+        fclose($fp);
+?>
+
 
 </body>
 </html> 
