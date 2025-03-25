c10c4e0d1338b6cd27a46df6ae4285fd412e14e2
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-25 12:25:23 -0400

    Update serialtest.php

--- serialtest.php
+++ serialtest.php
@@ -6,24 +6,6 @@
         $database = "finalproject";
         $conn = mysqli_connect($server, $username, $password, $database);
 
-       // $output = shell_exec('sudo rfcomm bind /dev/rfcomm0 98:D3:31:F6:92:6D 1');      
+        $output = shell_exec('sudo rfcomm bind /dev/rfcomm0 98:D3:31:F6:92:6D 1');      
 
-        $fp = fopen($bluetooth, "r");
-        if (!$fp) 
-        {
-                die("Not open");
-        }
-        
-        while(true)
-        {
-                $data = fgets($fp);
-                if($data)
-                {
-                        $data = trim($data);
-                        $sql = "INSERT INTO test (name, score) VALUES ('NULLY', $data);";            
-                        $result = mysqli_query($conn, $sql);                                    
-                }
-        }
-        $fclose($fp);
-        mysqli_close($conn); 
 ?>
