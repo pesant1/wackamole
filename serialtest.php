88c44c71002f79c7eb97cd02b68dd43170de3d95
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-31 12:28:58 -0400

    Update serialtest.php

--- serialtest.php
+++ serialtest.php
@@ -6,7 +6,7 @@
         $database = "finalproject";
         $conn = mysqli_connect($server, $username, $password, $database);
 
-        //$output = shell_exec('sudo rfcomm bind /dev/rfcomm0 98:D3:31:F6:92:6D 1');      
+        $output = shell_exec('sudo rfcomm bind /dev/rfcomm0 98:D3:31:F6:92:6D 1');      
 
         $fp = fopen($bluetooth, "r");
         if (!$fp) 
