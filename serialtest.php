6d6fb35df36b7a6893d44f647818b6de148e668c
pesant1 <143018428+pesant1@users.noreply.github.com>
2025-03-31 13:24:26 -0400

    Update serialtest.php

--- serialtest.php
+++ serialtest.php
@@ -17,7 +17,7 @@
         while(true)
         {
                // $data = fread($fp, 3);
-                $data = fgets($fp, 3);
+                $data = fgets($fp);
                 $ascii = hexdec($data);
                 
 
